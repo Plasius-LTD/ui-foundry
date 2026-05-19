b884949bc09887341f92bd17bb0a83a2c30dd8c8
Phillip Hounslow <development@plasius.co.uk>
2026-05-19 05:14:09 +0100

    chore: clean bootstrap artifacts

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,6 @@
 import "@testing-library/jest-dom/vitest";
+import { vi } from "vitest";
+
+if (typeof HTMLCanvasElement !== "undefined") {
+  vi.spyOn(HTMLCanvasElement.prototype, "getContext").mockImplementation(() => null);
+}
